fccffaa8ec19d2e3a8d06d2e1a23095e42f622e2
vesselov <vesselov.zhenya@gmail.com>
2018-02-18 23:01:26 +0500

    1

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -9,3 +9,11 @@ $(document).ready(function(){
    
 
 });
+$(document).ready(function(){
+  
+    $(".fa-search").click(function(){
+      $(".container, .input").toggleClass("active");
+      $("input[type='text']").focus();
+    });
+    
+  });
