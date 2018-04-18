f3ee436e30028a4c04c2c7e28c7fb44c280a148c
vesselov <vesselov.zhenya@gmail.com>
2018-04-18 13:44:26 +0500

    adjustment

--- docs/assets/scripts/app.js
+++ docs/assets/scripts/app.js
@@ -14,3 +14,14 @@ var instance = M.Tabs.init(el, options);
 $(document).ready(function() {
     $('.tabs').tabs();
 });
+
+function menuOpen() {
+    var elem = document.getElementById('menu');
+    var hidden = document.getElementsByClassName('list__link')
+
+    if (elem) {
+        elem = hidden.classList.add('active');
+        console.log(elem)
+    }
+
+}
