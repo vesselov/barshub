77317b20e3dbb45410c21dbb996e70b51bc244ae
vesselov <vesselov.zhenya@gmail.com>
2019-02-04 20:35:11 +0500

    review

--- docs/assets/scripts/app.js
+++ docs/assets/scripts/app.js
@@ -1,49 +1,50 @@
-
-function up(){
+function up() {
     var button = $('.c1');
-     $(window).on('scroll', () =>{
-         if($(this).scrollTop() >= 150){
-             button.fadeIn();
+    $(window).on('scroll', () => {
+        if ($(this).scrollTop() >= 150) {
+            button.fadeIn();
 
-         }else{
-             button.fadeOut();
-         }
-        
-   });
-   button.click(function(){
-       $('body,html').animate({scrollTop:0},2000)
-   })
+        } else {
+            button.fadeOut();
+        }
+
+    });
+    button.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 2000)
+    })
 }
 up();
 // create pager list items
 var sliderImage = $('.slider__images-image');
 
 sliderImage.each(function (index) {
-    $('.js__slider__pagers').append('<li>'+(index+1)+'</li>');
+    $('.js__slider__pagers').append('<li>' + (index + 1) + '</li>');
 });
 
 // set up vars
-var sliderPagers       = 'ol.js__slider__pagers li',
+var sliderPagers = 'ol.js__slider__pagers li',
     sliderPagersActive = '.js__slider__pagers li.active',
-    sliderImages       = '.js__slider__images',
-    sliderImagesItem   = '.slider__images-item',
-    sliderControlNext  = '.slider__control--next',
-    sliderControlPrev  = '.slider__control--prev',
-    sliderSpeed        = 5000,
-    lastElem           = $(sliderPagers).length-1,
+    sliderImages = '.js__slider__images',
+    sliderImagesItem = '.slider__images-item',
+    sliderControlNext = '.slider__control--next',
+    sliderControlPrev = '.slider__control--prev',
+    sliderSpeed = 5000,
+    lastElem = $(sliderPagers).length - 1,
     sliderTarget;
 
 // add css heigt to slider images list
 function checkImageHeight() {
-    var sliderHeight = $('.slider__images-image:visible').height(); 
-    $(sliderImages).css('height', sliderHeight+'px');
+    var sliderHeight = $('.slider__images-image:visible').height();
+    $(sliderImages).css('height', sliderHeight + 'px');
 };
 
-sliderImage.on('load', function() {
+sliderImage.on('load', function () {
     checkImageHeight();
     $(sliderImages).addClass('loaded')
 });
-$(window).resize(function(){
+$(window).resize(function () {
     checkImageHeight();
 });
 
@@ -58,34 +59,34 @@ function sliderResponse(sliderTarget) {
 }
 
 // pager controls
-$(sliderPagers).on('click', function() {
-    if ( !$(this).hasClass('active') ) {
+$(sliderPagers).on('click', function () {
+    if (!$(this).hasClass('active')) {
         sliderTarget = $(this).index();
         sliderResponse(sliderTarget);
         resetTiming();
     }
 });
 //search
-$(document).ready(function(){
-  
-    $(".fa-search").click(function(){
-      $(".container__search , .container__input").toggleClass("active");
-      $("input[type='text']").focus();
+$(document).ready(function () {
+
+    $(".fa-search").click(function () {
+        $(".container__search , .container__input").toggleClass("active");
+        $("input[type='text']").focus();
     });
-    
-  });
+
+});
 
 // next/prev controls
-$(sliderControlNext).on('click', function() {
+$(sliderControlNext).on('click', function () {
     sliderTarget = $(sliderPagersActive).index();
-    sliderTarget === lastElem ? sliderTarget = 0 : sliderTarget = sliderTarget+1;
+    sliderTarget === lastElem ? sliderTarget = 0 : sliderTarget = sliderTarget + 1;
     sliderResponse(sliderTarget);
     resetTiming();
 });
-$(sliderControlPrev).on('click', function() {
+$(sliderControlPrev).on('click', function () {
     sliderTarget = $(sliderPagersActive).index();
-    lastElem = $(sliderPagers).length-1;
-    sliderTarget === 0 ? sliderTarget = lastElem : sliderTarget = sliderTarget-1;
+    lastElem = $(sliderPagers).length - 1;
+    sliderTarget === 0 ? sliderTarget = lastElem : sliderTarget = sliderTarget - 1;
     sliderResponse(sliderTarget);
     resetTiming();
 });
@@ -93,35 +94,35 @@ $(sliderControlPrev).on('click', function() {
 // slider timing
 function sliderTiming() {
     sliderTarget = $(sliderPagersActive).index();
-    sliderTarget === lastElem ? sliderTarget = 0 : sliderTarget = sliderTarget+1;
+    sliderTarget === lastElem ? sliderTarget = 0 : sliderTarget = sliderTarget + 1;
     sliderResponse(sliderTarget);
 }
 
 // slider autoplay
-var timingRun = setInterval(function() {
+var timingRun = setInterval(function () {
     sliderTiming();
 }, sliderSpeed);
 
 function resetTiming() {
     clearInterval(timingRun);
-    timingRun = setInterval(function() {
-      sliderTiming();
+    timingRun = setInterval(function () {
+        sliderTiming();
     }, sliderSpeed);
 }
 
-$(document).ready(function(){
+$(document).ready(function () {
     var button = $('.menu-box')
     var menu = $('.list__link')
     var Blockmenu = $('.menu-block')
-    
-    
-    button.click(function(){
-        if (button){
-         Blockmenu.toggleClass('active') 
-         menu.toggleClass('active_link') 
-        }     
-})
-    if ($(window).width() >= 480){
+
+
+    button.click(function () {
+        if (button) {
+            Blockmenu.toggleClass('active')
+            menu.toggleClass('active_link')
+        }
+    })
+    if ($(window).width() >= 480) {
         Blockmenu.removeClass('active')
         menu.removeClass('active_link')
 
@@ -129,26 +130,28 @@ $(document).ready(function(){
     }
 });
 //стрелки на слайдере
-$(document).ready(function(){
+$(document).ready(function () {
     var nav = $('.slider__control ');
     var elem = $('.slider');
-    elem.hover(()=>{
-        nav.css('display', 'block');
-        
-        },
-        ()=>{
-            nav.css('display','none');
+    elem.hover(() => {
+            nav.css('display', 'block');
 
-    })
+        },
+        () => {
+            nav.css('display', 'none');
+
+        })
 });
 //на странице карта. переход в магазин
-$(document).ready(function(){
-    $('.card__button').click(()=>{
+$(document).ready(function () {
+    $('.card__button').click(() => {
         window.location = 'buy.html'
-    }) 
+    })
 })
-$(document).ready(function(){
-    $(".menu__container").on('click',function(){
-      $(this).find(".hambergerIcon").toggleClass("open");
+$(document).ready(function () {
+    $(".menu__container").on('click', function () {
+        $(this).find(".hambergerIcon").toggleClass("open");
     });
-  })
+})
+///////////// form
+
