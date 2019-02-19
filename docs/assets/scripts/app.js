56ab7cf4d344cf3e0450ce4e1b4ad6183c3aaf64
vesselov <vesselov.zhenya@gmail.com>
2019-02-19 19:29:24 +0500

    email

--- docs/assets/scripts/app.js
+++ docs/assets/scripts/app.js
@@ -154,4 +154,37 @@ $(document).ready(function () {
     });
 })
 ///////////// form
+var submitForm = function (ev) {
+    ev.preventDefault();
+   
 
+    var form = $(ev.target);
+        
+    var request = ajaxForm(form);
+
+    request.done(function(msg) {
+        var mes = msg.mes,
+            status = msg.status;
+        if (status === 'OK') {
+            form.append('<p class="success">' + mes + '</p>');
+        } else{
+            form.append('<p class="error">' + mes + '</p>');
+        }
+    });
+
+    request.fail(function(jqXHR, textStatus) {
+        alert("Request failed: " + textStatus);
+    });
+}
+
+var ajaxForm = function (form) {
+
+    var url = form.attr('action'),
+        data = form.serialize();
+
+    return $.ajax({
+        type: 'POST',
+        url: url,
+        data: data,
+        dataType: 'JSON'
+    });
